065e194c356e8b7f083f014e674cc342331880c8
Trim Bresilla <trim.bresilla@gmail.com>
2021-02-08 19:09:22 +0100

    FIX: modified LOC

--- src/syntax/index/mod.rs
+++ src/syntax/index/mod.rs
@@ -1,6 +1,4 @@
 pub mod source;
 pub mod sources;
-pub mod point;
 pub use crate::syntax::index::source::Source;
 pub use crate::syntax::index::sources::Sources;
-pub use crate::syntax::index::point::Point;
