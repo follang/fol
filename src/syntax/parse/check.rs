f0aa333088a110f8d23e81d159381ab445d41b1d
Trim Bresilla <trim.bresilla@gmail.com>
2021-03-06 18:39:26 +0100

    ADD & FIX: Added more into parsng Fixed checking login

--- src/syntax/parse/check.rs
+++ src/syntax/parse/check.rs
@@ -4,100 +4,107 @@ use crate::syntax::lexer;
 use crate::syntax::token::*;
 use crate::syntax::point;
 use crate::syntax::index;
+use crate::syntax::parse::eater;
 
 pub fn expect(lex: &mut lexer::Elements, keyword: KEYWORD, ignore: bool) -> Vod {
-    if lex.curr(ignore)?.key() == keyword {
-        return Ok(())
-    };
+    if lex.curr(ignore)?.key() == keyword { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
+    eater::until_term(lex, true)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: keyword,
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_many(lex: &mut lexer::Elements, keywords: Vec<KEYWORD>, ignore: bool) -> Vod {
-    let currkey = &lex.curr(ignore)?.key();
-    if let Some(_) = keywords.iter().find(|&x| x == currkey) {
-        return Ok(())
-    }
+    let wrong = lex.curr(ignore)?;
+    if let Some(_) = keywords.iter().find(|&x| x == &wrong.key()) { return Ok(()) }
     Err( catch!( Typo::ParserManyUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         keys: keywords,
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_ident(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Identifier) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Identifier, 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_ident_literal(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Identifier) || matches!(lex.curr(ignore)?.key(), KEYWORD::Literal(_)) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Identifier, 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_literal(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Literal(_)) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Literal(LITERAL::ANY), 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_buildin(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Keyword(_)) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Keyword(BUILDIN::ANY), 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_symbol(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Symbol(_)) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Symbol(SYMBOL::ANY), 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_operator(lex: &mut lexer::Elements, ignore: bool) -> Vod {
     if matches!(lex.curr(ignore)?.key(), KEYWORD::Operator(_)) { return Ok(()) };
+    let wrong = lex.curr(ignore)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(ignore)?.loc().clone()), 
-        key1: lex.curr(ignore)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Operator(OPERATOR::ANY), 
-        src: lex.curr(ignore)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_void(lex: &mut lexer::Elements) -> Vod {
     if matches!(lex.curr(false)?.key(), KEYWORD::Void(_)) { return Ok(()) };
+    let wrong = lex.curr(false)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(false)?.loc().clone()), 
-        key1: lex.curr(false)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Void(VOID::ANY), 
-        src: lex.curr(false)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn expect_terminal(lex: &mut lexer::Elements) -> Vod {
     if lex.curr(false)?.key().is_terminal() { return Ok(()) };
+    let wrong = lex.curr(false)?;
     Err( catch!( Typo::ParserUnexpected{ 
-        loc: Some(lex.curr(false)?.loc().clone()), 
-        key1: lex.curr(false)?.key(), 
+        loc: Some(wrong.loc().clone()), 
+        key1: wrong.key(), 
         key2: KEYWORD::Void(VOID::ANY), 
-        src: lex.curr(false)?.loc().source().clone()
+        src: wrong.loc().source().clone()
     }))
 }
 pub fn type_balance(idents: usize, dt: usize, loc: &point::Location, src: &Option<index::Source>) -> Vod {
