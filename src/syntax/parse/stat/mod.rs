90c15ec0046f05d239e141b234061593d6bfe7cf
Trim Bresilla <trim.bresilla@gmail.com>
2021-03-06 12:32:38 +0100

    ADD: added body branching

--- src/syntax/parse/stat/mod.rs
+++ src/syntax/parse/stat/mod.rs
@@ -4,7 +4,7 @@ use crate::syntax::nodes::Nodes;
 use crate::syntax::token::*;
 use crate::syntax::lexer;
 use super::Parse;
-use crate::syntax::parse::{check, Body};
+use crate::syntax::parse::{check, branch, Body};
 
 pub mod parameters;
 pub mod generics;
@@ -26,7 +26,7 @@ pub struct ParserStat {
 
 impl ParserStat {
     pub fn init() -> Self {
-        Self { nodes: Nodes::new(), _style: Body::Top} 
+        Self { nodes: Nodes::new(), _style: Body::Fun} 
     }
     pub fn style(&mut self, style: Body) {
         self._style = style;
@@ -35,20 +35,48 @@ impl ParserStat {
 impl Parse for ParserStat {
     fn nodes(&self) -> Nodes { self.nodes.clone() }
     fn parse(&mut self, lex: &mut lexer::Elements) -> Vod {
-        if lex.curr(true)?.key().is_assign()
-            || (matches!(lex.curr(true)?.key(), KEYWORD::Symbol(_)) && lex.peek(0, true)?.key().is_assign())
-        {
-            let mut parse_ass = ParserStatAss::init();
-            parse_ass.parse(lex)?;
-            self.nodes.extend(parse_ass.nodes);
-            return Ok(())
-        } else {
-            if let Err(_) = check::expect(lex,  KEYWORD::Illegal, true) {
-                // lex.debug(true, 0);
-                lex.until_term(false)?;
-                // return Err(e)
+        let token = lex.curr(true)?;
+        match self._style {
+            Body::Top => {
+                if (lex.curr(true)?.key().is_assign()
+                    || (matches!(lex.curr(true)?.key(), KEYWORD::Symbol(_)) && lex.peek(0, true)?.key().is_assign()))
+                    && branch::body_top(lex, true)? {
+                    let mut parse_ass = ParserStatAss::init();
+                    parse_ass.parse(lex)?;
+                    self.nodes.extend(parse_ass.nodes);
+                    return Ok(())
+                } else { return check::unexpected_top(lex, token); }
+            }
+            Body::Typ => {
+                if (lex.curr(true)?.key().is_assign()
+                    || (matches!(lex.curr(true)?.key(), KEYWORD::Symbol(_)) && lex.peek(0, true)?.key().is_assign()))
+                    && branch::body_typ(lex, true)? {
+                    let mut parse_ass = ParserStatAss::init();
+                    parse_ass.parse(lex)?;
+                    self.nodes.extend(parse_ass.nodes);
+                    return Ok(())
+                } else { return check::unexpected_typ(lex, token); }
+            }
+            Body::Imp => {
+                if (lex.curr(true)?.key().is_assign()
+                    || (matches!(lex.curr(true)?.key(), KEYWORD::Symbol(_)) && lex.peek(0, true)?.key().is_assign()))
+                    && branch::body_imp(lex, true)? {
+                    let mut parse_ass = ParserStatAss::init();
+                    parse_ass.parse(lex)?;
+                    self.nodes.extend(parse_ass.nodes);
+                    return Ok(())
+                } else { return check::unexpected_imp(lex, token); }
+            }
+            Body::Fun => {
+                if (lex.curr(true)?.key().is_assign()
+                    || (matches!(lex.curr(true)?.key(), KEYWORD::Symbol(_)) && lex.peek(0, true)?.key().is_assign()))
+                    && branch::body_fun(lex, true)? {
+                    let mut parse_ass = ParserStatAss::init();
+                    parse_ass.parse(lex)?;
+                    self.nodes.extend(parse_ass.nodes);
+                    return Ok(())
+                } else { return check::unexpected_fun(lex, token); }
             }
         }
-        Ok(())
     }
 }
