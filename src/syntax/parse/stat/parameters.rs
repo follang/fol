1130aeebef8f9ecd3c3cd291dab72575a6cdcd9c
Trim Bresilla <trim.bresilla@gmail.com>
2021-03-08 11:39:12 +0100

    FIX: comma insted of semicolon for generics

--- src/syntax/parse/stat/parameters.rs
+++ src/syntax/parse/stat/parameters.rs
@@ -4,7 +4,7 @@ use crate::syntax::token::*;
 use crate::syntax::lexer;
 use super::Parse;
 
-use crate::syntax::parse::{eater, check};
+use crate::syntax::parse::check;
 use crate::syntax::parse::stat::assign::opts::*;
 use crate::syntax::parse::stat::ident::*;
 use crate::syntax::parse::stat::datatype::*;
@@ -80,32 +80,28 @@ impl ParserStatParameters {
         let mut idents = ParserStatIdent::init();
         idents.once();
         idents.parse(lex)?; lex.eat();
+        node.set_ident(Some(idents.nodes.get(0).clone()));
 
         // match datatypes after :  -> "int[opts][]"
         let mut dt = ParserStatDatatypes::init();
         dt.once();
         dt.parse(lex)?;
+        if dt.nodes.len() > 0 { node.set_datatype(Some(dt.nodes.get(0).clone())); }
 
         check::expect_many(lex, vec![ 
             KEYWORD::Symbol(SYMBOL::Comma),
-            KEYWORD::Symbol(SYMBOL::Equal),
+            // KEYWORD::Symbol(SYMBOL::Equal),
             KEYWORD::Symbol(SYMBOL::RoundC),
             KEYWORD::Void(VOID::EndLine)
         ], true)?;
         check::type_balance(idents.nodes.len(), dt.nodes.len(), &loc, &lex.curr(false)?.loc().source() )?;
 
-        let mut nodes: Nodes = List::new();
-        for i in 0..idents.nodes.len() {
-            if dt.nodes.len() > 0 {
-                let idx = if i >= dt.nodes.len() { dt.nodes.len()-1 } else { i };
-                node.set_datatype(Some(dt.nodes.get(idx).clone()));
-            }
-            node.set_ident(Some(idents.nodes.get(i).clone()));
-            let id = Node::new(Box::new(node.clone()));
-            // id.set_loc(loc.clone());
-            nodes.push(id);
-        }
-        eater::until_key(lex, vec![KEYWORD::Symbol(SYMBOL::RoundC), KEYWORD::Symbol(SYMBOL::Comma)])?;
+        let nodes: Nodes = List::new();
+        let id = Node::new(Box::new(node));
+        // id.set_loc(loc.clone());
+        self.nodes.push(id);
+
+        // eater::until_key(lex, vec![KEYWORD::Symbol(SYMBOL::RoundC), KEYWORD::Symbol(SYMBOL::Comma)])?;
         Ok(nodes)
     }
 }
