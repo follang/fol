ca55ab05589e41a95f64e7b9969a32c09485e75b
Trim Bresilla <trim.bresilla@gmail.com>
2026-02-28 22:28:51 +0100

    test: verify ParseError carries token location
    Add parser integration coverage that forces an illegal token and asserts ParseError reports non-zero line, column, and length for diagnostics.

--- test/parser/test_parser.rs
+++ test/parser/test_parser.rs
@@ -1,8 +1,9 @@
 // Parser tests - to be expanded when full parser is implemented
 
-use fol_stream::FileStream;  
 use fol_lexer::lexer::stage3::Elements;
-use fol_parser::ast::{AstParser, AstNode};
+use fol_lexer::token::KEYWORD;
+use fol_parser::ast::{AstNode, AstParser, ParseError};
+use fol_stream::FileStream;
 
 #[cfg(test)]
 mod parser_tests {
@@ -10,15 +11,18 @@ mod parser_tests {
 
     #[test]
     fn test_basic_parsing() {
-        let mut file_stream = FileStream::from_file("test/parser/simple_var.fol")
-            .expect("Should read test file");
-        
+        let mut file_stream =
+            FileStream::from_file("test/parser/simple_var.fol").expect("Should read test file");
+
         let mut lexer = Elements::init(&mut file_stream);
         let mut parser = AstParser::new();
-        
+
         match parser.parse(&mut lexer) {
             Ok(ast) => {
-                assert!(matches!(ast, AstNode::Program { .. }), "Should return Program node");
+                assert!(
+                    matches!(ast, AstNode::Program { .. }),
+                    "Should return Program node"
+                );
                 println!("Successfully parsed AST: {:?}", ast);
             }
             Err(errors) => {
@@ -29,15 +33,18 @@ mod parser_tests {
 
     #[test]
     fn test_function_parsing() {
-        let mut file_stream = FileStream::from_file("test/parser/simple_fun.fol")
-            .expect("Should read test file");
-        
+        let mut file_stream =
+            FileStream::from_file("test/parser/simple_fun.fol").expect("Should read test file");
+
         let mut lexer = Elements::init(&mut file_stream);
         let mut parser = AstParser::new();
-        
+
         match parser.parse(&mut lexer) {
             Ok(ast) => {
-                assert!(matches!(ast, AstNode::Program { .. }), "Should return Program node");
+                assert!(
+                    matches!(ast, AstNode::Program { .. }),
+                    "Should return Program node"
+                );
                 println!("Successfully parsed function AST: {:?}", ast);
             }
             Err(errors) => {
@@ -50,36 +57,73 @@ mod parser_tests {
     #[test]
     fn test_literal_parsing() {
         let parser = AstParser::new();
-        
+
         // Test integer literal
         match parser.parse_literal("42") {
             Ok(ast) => {
-                assert!(matches!(ast, AstNode::Literal(_)), "Should parse integer literal");
+                assert!(
+                    matches!(ast, AstNode::Literal(_)),
+                    "Should parse integer literal"
+                );
             }
             Err(e) => panic!("Should parse integer literal: {:?}", e),
         }
-        
+
         // Test string literal
         match parser.parse_literal("\"hello\"") {
             Ok(ast) => {
-                assert!(matches!(ast, AstNode::Literal(_)), "Should parse string literal");
+                assert!(
+                    matches!(ast, AstNode::Literal(_)),
+                    "Should parse string literal"
+                );
             }
             Err(e) => panic!("Should parse string literal: {:?}", e),
         }
-        
+
         // Test identifier
         match parser.parse_literal("variable_name") {
             Ok(ast) => {
-                assert!(matches!(ast, AstNode::Identifier { .. }), "Should parse identifier");
+                assert!(
+                    matches!(ast, AstNode::Identifier { .. }),
+                    "Should parse identifier"
+                );
             }
             Err(e) => panic!("Should parse identifier: {:?}", e),
         }
+    }
+
+    #[test]
+    fn test_parse_error_has_location_for_illegal_token() {
+        let mut file_stream =
+            FileStream::from_file("test/parser/simple_var.fol").expect("Should read test file");
+
+        let mut lexer = Elements::init(&mut file_stream);
+        lexer
+            .set_key(KEYWORD::Illegal)
+            .expect("Should be able to force an illegal token for parser test");
+
+        let mut parser = AstParser::new();
+        let errors = parser
+            .parse(&mut lexer)
+            .expect_err("Parser should fail when current token is illegal");
+
+        let parse_error = errors
+            .first()
+            .and_then(|e| e.as_any().downcast_ref::<ParseError>())
+            .expect("First parser error should be ParseError");
+
+        assert!(parse_error.line() > 0, "Line should be non-zero");
+        assert!(parse_error.column() > 0, "Column should be non-zero");
+        assert!(
+            parse_error.length() > 0,
+            "Token length should be non-zero for diagnostics"
+        );
     }
 }
 
 // TODO: Expand these tests when full parser is implemented
 // - Variable declarations
-// - Function declarations  
+// - Function declarations
 // - Type declarations
 // - Expressions
 // - Statements
