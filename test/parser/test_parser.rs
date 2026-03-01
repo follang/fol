ba10b1f567b135c3e7780860436b39718af08a22
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-01 01:32:01 +0100

    test: cover mixed precedence with left associativity
    Add parser fixture and AST-shape assertion for a - b * c - d to lock precedence and associativity behavior in a single mixed expression case.

--- test/parser/test_parser.rs
+++ test/parser/test_parser.rs
@@ -394,6 +394,52 @@ mod parser_tests {
     }
 
     #[test]
+    fn test_return_expression_mixed_precedence_and_associativity() {
+        let mut file_stream =
+            FileStream::from_file("test/parser/simple_fun_mixed_precedence_assoc.fol")
+                .expect("Should read mixed precedence function test file");
+
+        let mut lexer = Elements::init(&mut file_stream);
+        let mut parser = AstParser::new();
+
+        let ast = parser
+            .parse(&mut lexer)
+            .expect("Parser should parse mixed precedence function");
+
+        let return_value = match ast {
+            AstNode::Program { declarations } => declarations
+                .iter()
+                .find_map(|node| {
+                    if let AstNode::Return { value: Some(value) } = node {
+                        Some(value.as_ref().clone())
+                    } else {
+                        None
+                    }
+                })
+                .expect("Program should contain a return value"),
+            _ => panic!("Expected program node"),
+        };
+
+        match &return_value {
+            AstNode::BinaryOp { op, left, right: _ } => {
+                assert!(matches!(op, fol_parser::ast::BinaryOperator::Sub));
+                assert!(
+                    matches!(
+                        left.as_ref(),
+                        AstNode::BinaryOp {
+                            op: fol_parser::ast::BinaryOperator::Sub,
+                            left: _,
+                            right
+                        } if matches!(right.as_ref(), AstNode::BinaryOp { op: fol_parser::ast::BinaryOperator::Mul, .. })
+                    ),
+                    "Expected (a - (b * c)) - d tree shape"
+                );
+            }
+            _ => panic!("Return value should be subtraction expression"),
+        }
+    }
+
+    #[test]
     fn test_literal_parsing() {
         let parser = AstParser::new();
 
