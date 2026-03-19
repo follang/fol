d7e9d7dc88100645b2134ca38f067a29e3c19ace
bresilla <bresilla@users.noreply.github.com>
2026-03-19 09:08:39 +0000

    deploy: 69e5bcbd8cd1c68506534ca7dd21c9f444c7601f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-26cdcf2c.js',
+                path_to_root + 'searchindex-28302f77.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
