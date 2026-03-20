c3bc1b73218e952a2261170c0c13c930583cc601
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 10:44:34 +0000

    test(editor): trim navigation imports

--- lang/tooling/fol-editor/src/lsp/tests/navigation.rs
+++ lang/tooling/fol-editor/src/lsp/tests/navigation.rs
@@ -1,15 +1,9 @@
 use super::helpers::{open_document, sample_loc_workspace_root, sample_package_root};
 use super::super::{
-    completion_helpers::completion_context, completion_helpers::CompletionContext,
-    EditorLspServer, JsonRpcId, JsonRpcNotification, JsonRpcRequest, LspCompletionContext,
-    LspCompletionList, LspCompletionParams, LspDefinitionParams,
-    LspDidChangeTextDocumentParams, LspDidCloseTextDocumentParams,
-    LspDidOpenTextDocumentParams, LspDocumentSymbolParams, LspHover, LspHoverParams,
-    LspInitializeResult, LspLocation, LspPosition, LspPublishDiagnosticsParams,
-    LspTextDocumentContentChangeEvent, LspTextDocumentIdentifier, LspTextDocumentItem,
-    LspVersionedTextDocumentIdentifier,
+    EditorLspServer, JsonRpcId, JsonRpcRequest, LspDefinitionParams, LspDocumentSymbolParams,
+    LspLocation, LspPosition, LspTextDocumentIdentifier,
 };
-use crate::{EditorConfig, EditorDocument, EditorDocumentUri};
+use crate::EditorConfig;
 use std::fs;
 use std::path::PathBuf;
 
